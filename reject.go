6d1da54a79d52549518a663d90d97661369eed67
Jon Ruskin <jon.ruskin@gmail.com>
2024-03-27 20:11:42 -0700

    opt: minimize reject
    cmon, it's really just a negated select...
    
    I think I made this a separate implementation from select for a different attempt at implementing panic and stats handling :shrug:

--- reject.go
+++ reject.go
@@ -1,56 +1,15 @@
 package channels
 
-import (
-	"time"
-
-	"github.com/jonabc/channels/providers"
-)
-
-type RejectConfig struct {
-	panicProvider providers.Provider[any]
-	statsProvider providers.Provider[SelectStats]
-	capacity      int
-}
-
 // Selects values from the input channel that return false from the provided `rejectFn`
 // and pushes them to the output channel.  The output channel is unbuffered by default,
 // and is closed once the input channel is closed and all selected values pushed to the output channel.
-func Reject[T any](inc <-chan T, rejectFn func(T) bool, opts ...Option[RejectConfig]) <-chan T {
-	cfg := parseOpts(opts...)
-
-	outc := make(chan T, cfg.capacity)
-	panicProvider := cfg.panicProvider
-	statsProvider := cfg.statsProvider
-
-	go func() {
-		defer tryHandlePanic(panicProvider)
-		defer close(outc)
-
-		for in := range inc {
-			start := time.Now()
-			selected := !rejectFn(in)
-			duration := time.Since(start)
-
-			if selected {
-				outc <- in
-			}
-
-			tryProvideStats(SelectStats{Duration: duration, Selected: selected}, statsProvider)
-		}
-	}()
-
-	return outc
+func Reject[T any](inc <-chan T, rejectFn func(T) bool, opts ...Option[SelectConfig]) <-chan T {
+	return Select(inc, func(t T) bool { return !rejectFn(t) }, opts...)
 }
 
 // Like Reject, but blocks until the input channel is closed and all values are read.
 // RejectValues reads all values from the input channel and returns an array of values
 // that return false from the provided `rejectFn` function.
-func RejectValues[T any](inc <-chan T, rejectFn func(T) bool, opts ...Option[RejectConfig]) []T {
-	outc := Reject(inc, rejectFn, opts...)
-	result := make([]T, 0, len(inc))
-	for out := range outc {
-		result = append(result, out)
-	}
-
-	return result
+func RejectValues[T any](inc <-chan T, rejectFn func(T) bool, opts ...Option[SelectConfig]) []T {
+	return SelectValues(inc, func(t T) bool { return !rejectFn(t) }, opts...)
 }
